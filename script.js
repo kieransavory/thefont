410abd780b541ce2fee5ab18d6bd18bc2b644e76
Kieran Savory <96089380+kieransavory@users.noreply.github.com>
2023-01-07 20:20:23 +0000

    Changes to menu functionality and music link modals.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//This function displays a div of a video if flash is selected.
+//Function displays a div of a video on landing page if flash is selected.
 function showFlash(){
 
     let warningScreen = document.getElementById('warning');
@@ -14,7 +14,7 @@ function showFlash(){
 
 }
 
-//This function displays a div of a still image if no flash is selected.
+//Function displays a div of a still image on landing page if no flash is selected.
 function hideFlash() {
 
     let warningScreen = document.getElementById('warning');
@@ -27,7 +27,7 @@ function hideFlash() {
 
 }
 
-//This function returns user to top of page.
+//Function returns user to top of page.
 function topScroll() {
     scrollTo(0,0);
 }
@@ -39,21 +39,19 @@ function scrollToHeroTwo() {
 }
 
 function navSize() {
-
 //Check user device for mobile or non mobile.
 
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     const image = document.getElementById('navbarimage');
     let scroll = window.scrollY;
-
-//If is not mobile, desktop styling applies.
+//Applies styling based on position of navbar relative to amount user has scrolled AND whether or not user is on mobile device or computer. 
 
     if (!isMobile) {
 
         if (scroll > 500) {
             image.style.width = "10%";
-            image.style.opacity = "50%";
+            image.style.opacity = "100%";
             image.style.position = "absolute";
             image.style.top = "10px";
 
@@ -83,18 +81,57 @@ function navSize() {
 }
 
 
-
+//Function checks for user input and opens navigational menu accordingly. 
 function openNavMenu() {
     let menu = document.getElementById('navMenu');
     menu.classList.toggle('active');
 }
 
+//Function scrolls to top of page. 
 function homeMenuScroll() {
     topScroll();
     openNavMenu();
 }
 
+//Function scrolls to listen div. 
 function listenMenuScroll() {
     scrollToHeroTwo();
     openNavMenu();
+}
+
+//Function changes background art of listen div based on hover of child element. 
+function bgArtMatador() {
+
+    let divBackground = document.getElementById('heroTwo');
+    divBackground.style.backgroundImage = "url(matadorbg.jpg)";
+    
+    let matador = document.getElementById('matadorArt');
+    matador.addEventListener('mouseleave', () => {
+        divBackground.style.backgroundImage = "none";
+    })
+
+}
+
+//Function changes background art of listen div based on hover of child element. 
+function bgArtFourWalls() {
+
+    let divBackground = document.getElementById('heroTwo');
+    divBackground.style.backgroundImage = "url(fourwallsbg2.jpg)";
+
+    
+    let fourWalls = document.getElementById('fourWallsArt');
+    fourWalls.addEventListener('mouseleave', () => {
+        divBackground.style.backgroundImage = "none";
+    })
+
+}
+
+function modalActiveOne() {
+    let modal = document.getElementById('listModalOne');
+    modal.classList.toggle('active');
+}
+
+function modalActiveTwo() {
+    let modal = document.getElementById('listModalTwo');
+    modal.classList.toggle('active');
 }
